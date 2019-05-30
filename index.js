432d0846a730e069190bb1cc424ac299d0f7cdbb
Jonathan Taylor <jonyonson@icloud.com>
2019-05-30 16:54:55 -0500

    front end app working

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 
 const server = require('./server.js');
 
-// handled by dotenv or heroku
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const server = express();
 
 const dadJokes = require('./data/dadJokes.js');
 const devJokes = require('./data/devJokes.js');
 
+// Middleware
 server.use(helmet());
 server.use(express.json());
+server.use(cors({ origin: 'http://localhost:3000' }));
 
 server.get('/', (req, res) => {
   res.send('<h2>Jokes API</h2>');
@@ -21,6 +24,12 @@ server.get('/dad-jokes', (req, res) => {
 server.get('/dev-jokes', (req, res) => {
   const randomDevJoke = devJokes[Math.floor(Math.random() * devJokes.length)];
   res.status(200).send(randomDevJoke);
+});
+
+server.get('/all-jokes', (req, res) => {
+  const allJokes = dadJokes.concat(devJokes);
+  const randomJoke = allJokes[Math.floor(Math.random() * allJokes.length)];
+  res.status(200).send(randomJoke);
 });
 
 module.exports = server;
